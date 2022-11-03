fed124a73dc61ee8d3972468060664848dd42181
Gosia Stepaniak <g.a.stepaniak@gmail.com>
2022-11-03 16:44:21 +0100

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryOfObjects = galleryItems
+    .map((image) => `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+            <img
+                 class="gallery__image"
+                src="${image.preview}"
+                data-source="${image.original}"
+                alt="${image.description}"/>
+        </a>
+    </div>`)
+    .join("");
+gallery.insertAdjacentHTML("afterbegin", galleryOfObjects);
+gallery.addEventListener("click", (event) =>
+    // event.currentTarget.src = data-source
+);
 
-console.log(galleryItems);
+
+// console.log(galleryOfObjects);
